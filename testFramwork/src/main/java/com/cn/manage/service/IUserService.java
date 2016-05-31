c2d44928fa7f071d88a426b703bdf12a522763eb
baizu <baizu2015@gmail.com>
2016-05-31 22:16:45 +0800

    delete files

--- testFramwork/src/main/java/com/cn/manage/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.cn.manage.service;
-
-import com.cn.manage.pojo.User;
-
-public interface IUserService {
-	public User getUserById(Integer id);
-	
-	public User getByUsername(String userName);
-}
--- testFramwork/src/main/resources/com/cn/manage/sql/createDatabase.sql
+++
@@ -1,2 +0,0 @@
-create database if not exists manage_database 
-default charset utf8 collate utf8_general_ci;
